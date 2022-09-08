b05474f459f75c98d5b1e42dcb35db3d1a05d8f1
Your Name <you@example.com>
2022-09-08 16:55:34 -0700

    updated puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+/*The main prints the line 'Programming is like building a mutlilingual puzzle'*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
