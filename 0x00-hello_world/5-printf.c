8986e5697232af0f29c238b55fa7d809af1054e2
Your Name <you@example.com>
2022-09-08 17:46:30 -0700

    5 printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints phrase provided
- * Return: if exited properly 0, 
+ * Main- prints line provided
+ * Return- exits correctly if 0 else non-zero
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
