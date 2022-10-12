9812736edd13f4de6ec910c9a652a16ad480c3f6
Hugh Muraya <hughmuraya@gmail.com>
2022-10-12 02:18:25 -0700

    changes to new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,7 +10,7 @@
 int _strlen(char *s)
 {
 	int i;
-	
+
 	i = 0;
 
 	while (s[i] != '\0')
@@ -34,7 +34,7 @@ char *_strcpy(char *dest, char *src)
 	int len, i;
 
 	len = 0;
-	
+
 	while (src[len] != '\0')
 	{
 		len++;
@@ -83,7 +83,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
-	
+
 	dog->age = age;
 
 	return (dog);
