95e232469e67ad0e2266a93f513c3c0380bd36bb
Hugh Muraya <hughmuraya@gmail.com>
2022-10-12 02:26:55 -0700

    changes on dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,14 +17,14 @@ struct dog
 };
 
 /**
- * dog - typedef for struct dog
+ * dog_t - typedef for struct dog
  */
 
-typedef struct dog dog;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
